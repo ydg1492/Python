c9d660194759a45111df8c3e83e7d9557606a3e8
ydg1492 <ydg57013@gmail.com>
2026-05-22 10:54:07 +0900

    modify

--- Submit/csv_output_panda.py
+++ Submit/csv_output_panda.py
@@ -1,13 +1,26 @@
+# import pandas as pd
+
+# df = pd.read_csv("Submit/scores.csv")
+# df.index = range(1, len(df) + 1)
+
+# df["총점"] = df[["국어", "영어", "수학"]].sum(axis=1)
+# df["평균"] = df[["국어", "영어", "수학"]].mean(axis=1).round(2)
+
+# result = df[["이름", "총점", "평균"]]
+
+# print(result.to_string(index=False))
+
+# result.to_csv("Submit/result.csv", index=False, encoding="utf-8-sig")
+
+
 import pandas as pd
 
 df = pd.read_csv("Submit/scores.csv")
-df.index = range(1, len(df) + 1)
 
-df["총점"] = df[["국어", "영어", "수학"]].sum(axis=1)
-df["평균"] = df[["국어", "영어", "수학"]].mean(axis=1).round(2)
+df["총점"] = df["국어"] + df["영어"] + df["수학"]
+df["평균"] = (df["총점"] / 3).round(2)
 
 result = df[["이름", "총점", "평균"]]
 
-print(result.to_string(index=False))
-
 result.to_csv("Submit/result.csv", index=False, encoding="utf-8-sig")
+print(result)
